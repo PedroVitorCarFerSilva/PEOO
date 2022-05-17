5d10b5038b621695bde2481aa680491927827f44
TaturanaGames <93941657+PedroVitorCarFerSilva@users.noreply.github.com>
2022-05-17 12:45:03 +0000

    exercicio 1 da lista 4

---
+++ lista4/ex1.cs
@@ -0,0 +1,20 @@
+using System;
+class uri1012{
+  static void Main(){
+    string selma = Console.ReadLine();
+    string[] dantas = selma.Split();
+    double a = double.Parse(dantas[0]);
+    double b = double.Parse(dantas[1]);
+    double c = double.Parse(dantas[2]);
+    double tri = a*c;
+    double cir = 3.14159*Math.Pow(c, 2);
+    double tra = ((a+b)*c)/2;
+    double qua = b*b;
+    double ret = a*b;
+    Console.WriteLine($"TRIANGULO: {tri:0.000}");
+    Console.WriteLine($"CIRCULO: {cir:0.000}");
+    Console.WriteLine($"TRAPEZIO: {tra:0.000}");
+    Console.WriteLine($"QUADRADO: {qua:0.000}");
+    Console.WriteLine($"RETANGULO: {ret:0.000}");
+  }
+}
