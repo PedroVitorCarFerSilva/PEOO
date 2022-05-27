ab9f24d300724b6f4472927673c4284405a18b23
TaturanaGames <93941657+PedroVitorCarFerSilva@users.noreply.github.com>
2022-05-27 10:17:05 +0000

    exercício 11 da lista de revisão 2

---
+++ listaR2/ex11.cs
@@ -0,0 +1,51 @@
+using System;
+class exercicio11{
+  static void Main(){
+    Console.WriteLine("Digite uma data no formato dd/mm/aaaa");
+    string data = Console.ReadLine();
+    string[] datas = data.Split("/");
+    int dia = int.Parse(datas[0]);
+    int mesnum = int.Parse(datas[1]);
+    int ano = int.Parse(datas[2]);
+    string mes = "<mês inválido>";
+    switch (mesnum) {
+      case 1:
+        mes = "janeiro";
+        break;
+      case 2:
+        mes = "fevereiro";
+        break;
+      case 3:
+        mes = "março";
+        break;
+      case 4:
+        mes = "abril";
+        break;
+      case 5:
+        mes = "maio";
+        break;
+      case 6:
+        mes = "junho";
+        break;
+      case 7:
+        mes = "julho";
+        break;
+      case 8:
+        mes = "agosto";
+        break;
+      case 9:
+        mes = "setembro";
+        break;
+      case 10:
+        mes = "outubro";
+        break;
+      case 11:
+        mes = "novembro";
+        break;
+      case 12:
+        mes = "dezembro";
+        break;
+    }
+    Console.WriteLine($"A data é {dia} de {mes} de {ano}");
+  }
+}
